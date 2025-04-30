8e4e0d42ef771505f42cf213f976233118d4d8cc
AvishkaNavindu <navinduavishka2002dfuze@gmail.com>
2025-04-30 17:52:33 +0530

    also changed colour
    to main colour

--- Project/Frontend/src/main.jsx
+++ Project/Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-// Create the root element  and render the App component inside it 
+// Create the root element   
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
